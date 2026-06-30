1b5ac435a906917e8daedb4a58242c01a9dd79ac
Marek Bełz <marazm.lab@gmail.com>
2026-06-30 20:44:25 +0200

    feat(about): implement about page with i18n and owner photo

---
+++ src/data/about-owner.ts
@@ -0,0 +1,9 @@
+export type AboutOwnerPhoto = {
+  src: string;
+  altKey: "about.owner.alt";
+};
+
+export const aboutOwnerPhoto: AboutOwnerPhoto = {
+  src: "/images/about/owner.jpg",
+  altKey: "about.owner.alt",
+};
